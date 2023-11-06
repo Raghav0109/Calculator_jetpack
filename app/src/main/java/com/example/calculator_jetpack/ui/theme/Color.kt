0a990d19a14603cc5d0847a79b40c44890065322
Raghav0109 <sawantra004@gmail.com>
2023-11-06 14:28:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculator_jetpack/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.calculator_jetpack.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Teal200 = Color(0xFF03DAC5)
+val Purple700 = Color(0xFF3700B3)
+val Purple500 = Color(0xFF6200EE)
+val Purple200 = Color(0xFFBB86FC)
+val Orange = Color(0xFFFF9800)
+val MediumGray = Color(0xFF2E2E2E)
+val LightGray = Color(0xFFA18181)
+val PurpleGrey80=Color(0xFF03DAD8)
+val Pink80=Color(0xFFF3DAC6)
+val Purple40=Color(0xFFFFF8C6)
+val PurpleGrey40=Color(0xFF03DDA8)
+val Pink40=Color(0xFFF5AAA9)
+val Purple80=Color(0xFFA9ABC9)
